9d5cc1e593fafa1221eaad408cc8799795dbe470
Francisco Silveira <86420413+FranciscoSSN@users.noreply.github.com>
2023-11-09 14:40:58 +0000

    add POST

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 app.get('/', function(req, res) {
     res.send('Chico')
 })
@@ -12,7 +14,24 @@ app.get('/oi', function(req, res){
 const list = ["Chico", "Francisco", "Flavia", "Raquel"]
 
 app.get('/item', function(req, res) {
-    res.send("lendo todos")
+    res.send(list)
+})
+
+app.get('/item/:id', function(req, res){
+    const id = req.params.id - 1
+    const item = list[id]
+
+    res.send(item)
+})
+
+// adicionado novo dado na memoria da lista
+app.post('/item', function(req, res) {
+    const item = req.body.nome
+
+    list.push(item)
+
+    res.send("Adicionado com sucesso")
+
 })
 
 app.listen(3000, () => console.log("rodando"))
